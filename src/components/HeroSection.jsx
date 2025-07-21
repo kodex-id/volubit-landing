a056c540bf1649910590d711b284b47e7f6c4a9e
Alif Aurii <auriimedia@gmail.com>
2025-07-22 02:07:37 +0700

    final: edit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import './HeroSection.css';
 
-function VolubitLogo() {
-  return (
-    <div className="hero-logo">
-      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M32 4L36.5 20.5H51L39 29.5L43.5 46L32 36.5L20.5 46L25 29.5L13 20.5H27.5L32 4Z" fill="#00CFFF"/>
-      </svg>
-    </div>
-  );
-}
-
 export default function HeroSection() {
   return (
     <section className="hero-section">
-      <VolubitLogo />
+      {/* Hapus logo bintang di sini */}
       <div className="hero-content">
         <div className="hero-badge">The Future of Web3 Education</div>
         <h1 className="hero-title">
